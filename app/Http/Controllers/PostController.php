58a9931fc2231bff0823adc3705a94f974c4fb4b
Hugo Anton Righolt <hugorigholt@icloud.com>
2026-06-29 19:11:18 +0200

    Fix PostController middleware issue
    - Removed middleware() call from constructor
    - Middleware is already defined in routes

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,11 +9,6 @@ use Inertia\Inertia;
 
 class PostController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
     public function index()
     {
         $posts = Post::where('published_at', '!=', null)
